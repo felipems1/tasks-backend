c90501ab39add054681963552eaa2366c6e39bad
felipe <083felype@gmail.com>
2024-08-03 01:08:27 -0300

    feat: add get task by id route

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { TaskDto } from './task.dto'
 
 @Injectable()
 export class TaskService {
+  private tasks: TaskDto[] = []
+
   create(task: TaskDto) {
-    console.log(task)
+    this.tasks.push(task)
+  }
+
+  findById(id: string): TaskDto {
+    const foundTask = this.tasks.filter((task) => task.id === id)
+
+    if (foundTask.length) {
+      return foundTask[0]
+    }
+
+    throw new HttpException(
+      `Task with id ${id} not found`,
+      HttpStatus.NOT_FOUND,
+    )
   }
 }
