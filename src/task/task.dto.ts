b2fb7d34c2c363f88a7066ee12417ebe2febe23d
felipe <083felype@gmail.com>
2024-08-03 20:55:59 -0300

    feat: add validation in taskDTO

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,8 +1,38 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator'
+
+export enum TaskStatusEnum {
+  TO_DO = 'TO_DO',
+  COMPLETED = 'COMPLETED',
+}
+
 export class TaskDto {
+  @IsUUID()
+  @IsOptional()
   id: string
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   title: string
+
+  @IsString()
+  @MinLength(5)
+  @MaxLength(512)
   description: string
+
+  @IsEnum(TaskStatusEnum)
+  @IsOptional()
   status: string
+
+  @IsDateString()
   deadline: Date
 }
 
